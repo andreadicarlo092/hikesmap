ed84d1865706ed74e63cbcac852bcf859b7e79ab
andreadicarlo092 <andreadicarlo092@gmail.com>
2026-09-20 20:37:09 +0200

    feat(etl): add db.py

---
+++ etl/db.py
@@ -0,0 +1,25 @@
+import psycopg2
+import psycopg2.extras
+from config import DB_HOST, DB_PORT, DB_NAME, DB_USER, DB_PASSWORD
+
+
+def get_connection():
+    return psycopg2.connect(
+        host=DB_HOST,
+        port=DB_PORT,
+        dbname=DB_NAME,
+        user=DB_USER,
+        password=DB_PASSWORD,
+    )
+
+
+def execute(conn, sql, params=None):
+    with conn.cursor() as cur:
+        cur.execute(sql, params)
+    conn.commit()
+
+
+def fetchall(conn, sql, params=None):
+    with conn.cursor(cursor_factory=psycopg2.extras.RealDictCursor) as cur:
+        cur.execute(sql, params)
+        return cur.fetchall()
